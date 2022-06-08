9f06d2b10d3b730dd13e82d182436f0e0bcdfdaf
Paaless <goth929@gmail.com>
2022-06-08 21:01:29 +0300

    creating the mvc

---
+++ Web/php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'palie/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6e039d4c972f7f75a9aad8a9fc2e9c7937cd9a52',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'palie/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6e039d4c972f7f75a9aad8a9fc2e9c7937cd9a52',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
