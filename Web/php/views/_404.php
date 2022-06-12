ab1c6de0a13f4e2814c8acca8b84230b51e636f3
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-06-12 16:56:06 +0300

    Made the 404 page

--- Web/php/views/_404.php
+++ Web/php/views/_404.php
@@ -4,15 +4,15 @@
 
 <html lang="en">
 <head>
-    <title>Home</title>
+    <title>404</title>
 
     <link rel="icon" type="image/x-icon" href="../media/Logos/final/pptext.svg"/>
-    <link rel="stylesheet" href="../css/index.css"/>
+    <link rel="stylesheet" href="../css/404.css"/>
     <link rel="stylesheet" href="../css/navbar.css"/>
 
     <meta charset="UTF-8"/>
     <meta name="author" content="Claudiu Strimbei"/>
-    <meta name="description" content="Home Page"/>
+    <meta name="description" content="404 page"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
@@ -77,38 +77,13 @@
 
     <div id="main">
         <div id="text-container">
-            <div id="title">
-                <p>Welcome to Picture Point!</p>
-            </div>
-
             <div class="text">
-                <p>Are you tired of searching on various media platforms whenever you want to see one of your pictures?</p>
-
-                <p>Do you have to keep track of too many social accounts to see your image stats?</p>
-
-                <p>Then this is where our app comes in to help you!</p>
+                <p>404 : Not found!</p>
             </div>
-
-            <a class="text-button" href="/Web-Project/Web/php/public/index.php/login">
-                <p>Get Started</p>
-            </a>
-
-            <div class="text">
-                <p>With Picture Point, you can gather all of your photos in one place.</p>
-
-                <p>
-                    Connect to some of your favorite social media platforms and import all of your pictures,
-                    or upload them directly from your device.
-                </p>
-
-                <p>View, download, edit and check the stats of your posted photos in real time.</p>
-            </div>
-
-            <a class="text-button" href="/Web-Project/Web/php/public/index.php/information">
-                <p>Learn More</p>
-            </a>
         </div>
     </div>
+</div>
+
 </div>
 </body>
 </html>
