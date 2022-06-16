472ac8b736e94d0cb1f2574c5d21086c70db8688
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-06-16 20:32:20 +0300

    Made a generic picture uploader from user's system

---
+++ imageUpload/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Image thing</title>
+</head>
+<body>
+<h1>Upload an image.</h1>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="username" name="username">
+    <button type="submit" name="submit">UPLOAD</button>
+
+</form>
+</body>
+</html>
