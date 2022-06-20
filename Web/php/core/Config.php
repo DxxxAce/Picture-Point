4a4ac8afb3680962659cf3e2182211713eeb7020
Alin Hîrtopanu <alinhirtopanu@gmail.com>
2022-06-20 19:05:10 +0300

    erased bad commit

--- Web/php/core/Config.php
+++ Web/php/core/Config.php
@@ -4,8 +4,8 @@ class Config
 {
     public array $config = [
         'userClass' => \app\models\User::class,
-        'db' =>["dsn" => "mysql:host=localhost;port=3306;dbname=web;charset=utf8",
-        "user" => "clodel",
-        "pass" => "claudiu001"]
+        'db' =>["dsn" => "mysql:host=localhost;port=3306;dbname=tudor;charset=utf8",
+        "user" => "tudor",
+        "pass" => "1234"]
     ];
 }
